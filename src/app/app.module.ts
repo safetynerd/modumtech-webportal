733cc3c38853f8ddc5c9e141878b55afc544861d
Uma2401 <59562868+Uma2401@users.noreply.github.com>
2022-08-09 19:31:19 +0530

    Revert "angular code setup"
    This reverts commit ad4aa66cf9fdaec90f042cfc50757d87c2666475.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { ApartmentComponent } from './restrictedAppraisal/apartment/apartment.component';
-import { LandComponent } from './restrictedAppraisal/land/land.component';
-import { OthersComponent } from './restrictedAppraisal/others/others.component';
-import { ImageCropperModule } from 'ngx-image-cropper';
-import { ActiveJobsListComponent } from './activeJobsList/active-jobs-list/active-jobs-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    ApartmentComponent,
-    LandComponent,
-    OthersComponent,
-    ActiveJobsListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    ImageCropperModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
